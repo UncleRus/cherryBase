57bddaa4593771bb6737c3b804b8f75a3a8820cc
rus <rus@rc-online.org>
2013-06-04 17:04:25 +0600

    sqlite

---
+++ src/cherrybase/db/drivers/sqlite.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+from ..base import ThreadedPool
+
+class Sqlite (ThreadedPool):
+
+    def __init__ (self, min_connections = 0, max_connections = 40, *args, **kwargs):
+        super (Sqlite, self).__init__ (sqlite3.connect, min_connections, max_connections, **kwargs)
